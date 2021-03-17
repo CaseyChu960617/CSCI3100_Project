275ff595b071f6f792d7a354152f3c1a1cadc346
kenny613 <tongkai0613@gmail.com>
2021-03-18 01:33:01 +0800

    update tutorials models

---
+++ backend/models/tutorial.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const user = require('../models/user')
+const chapter = require('../models/chapter')
+const tutorReply = require('../models/tutorReply')
+
+const tutorialschema = new mongoose.Schema({
+    author: {type: Schema.Types.ObjectId, ref: 'user' ,required: true, },
+    chapter: [{ type: Schema.Types.ObjectId, ref: 'chapter' ,required: true, }],
+    tutorReply:  [{ type: Schema.Types.ObjectId, ref: 'tutorReply' ,required: true, }],
+    createDate:  { type: Date, required: true },
+    subject: { type: String, required: true },
+    
+}, { collection: 'tutorial'});
+
+const tutorialModel = mongoose.model('tutorial', tutorialschema);
+module.exports = tutorialModel;
+
