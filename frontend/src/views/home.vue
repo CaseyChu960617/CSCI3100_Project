e34154ee4f02b74451e13fe42b3098c32377467a
michaelL21 <leung3342@yahoo.com.hk>
2021-03-17 19:24:27 +0800

    sign in ui

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-container v-if="currentUser">Home {{ message }}</v-container>
+    <Sign v-else />
+  </div>
+</template>
+
+<script>
+import Sign from "../components/auth/sign.vue";
+
+export default {
+  components: {
+    Sign,
+  },
+  data() {
+    return {
+      message: "abc",
+    };
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+};
+</script>
