544d35e29408a8cb85b621f5f2725d109efdd0c1
michaelL21 <leung3342@yahoo.com.hk>
2021-03-19 00:48:44 +0800

    tutorial+discussion skeleton

--- frontend/src/views/discussion.vue
+++ frontend/src/views/discussion.vue
@@ -1,33 +1,49 @@
 <template>
-  <v-container fluid>
+  <v-container fluid class="pa-0">
     <v-row no-gutters>
       <v-spacer />
-      <v-col md="3">
-        <v-card tile flat outlined>
-          <v-card-text>1</v-card-text>
-        </v-card>
-        <v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined> <v-card-text>1</v-card-text> </v-card
-        ><v-card tile flat outlined>
-          <v-card-text>1</v-card-text>
+      <v-col md="3" class="hidden-sm-and-down">
+        <v-card elevation="16">
+          <v-card-title>Discussion Thread</v-card-title>
+          <v-divider />
+          <v-virtual-scroll
+            bench="25"
+            min-height="83vh"
+            max-height="83vh"
+            item-height="85"
+            :items="items"
+          >
+            <template v-slot:default="{ item }">
+              <v-list-item :key="item">
+                <v-list-item-content>
+                  <v-list-item-title class="text-wrap">
+                    <strong>ID {{ item }}</strong>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider />
+            </template>
+          </v-virtual-scroll>
         </v-card>
       </v-col>
-      <v-col md="6"
-        ><v-card tile flat outlined height="100%">
-          <v-card-text>Card 2</v-card-text>
+      <v-col md="6" sm="12"
+        ><v-card tile elevation="16" outlined height="100%">
+          <v-card-text>content</v-card-text>
         </v-card></v-col
       ><v-spacer />
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    items() {
+      return Array.from({ length: 20 }, (k, v) => v + 1);
+    },
+  },
+};
+</script>
