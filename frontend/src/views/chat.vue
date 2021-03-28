414a12570c2e59c125c369a0e4d48313de54d251
kenny613 <tongkai0613@gmail.com>
2021-03-29 02:37:30 +0800

    Update chatbox layout

--- frontend/src/views/chat.vue
+++ frontend/src/views/chat.vue
@@ -1,61 +1,71 @@
 <template>
   <v-container fluid class="pa-0">
-    <div>
-      <p>Username: {{ currentUser.username }}</p>
+    <v-card
+      style="max-width:1200px; height:500px;overflow-y: scroll; width: 100%; padding: 12px;margin-right: auto;margin-left: auto; "
+      class="scroll-bar blue-grey lighten-5 nl-auto mr-auto"
+      elevation="16"
+      v-bind:style="messages.length == 0 ? 'display:flex' : ''"
+      ><!--max-width:400px;-->
 
       <v-container
-        style="max-width:1200px; height:500px;overflow-y: scroll;"
-        class="scroll-bar"
-        ><!--max-width:400px;-->
-
-        <v-row justify="space-around d-flex flex-column">
-          <v-card v-for="message in messages" :key="message.time" flat>
-            <v-list-item
-              :key="message.time"
-              v-if="message.from != 'You'"
-              class=""
-            >
-              <v-list-item-avatar class="align-self-start mr-2">
-                <v-avatar size="40">
-                  <v-img src="https://via.placeholder.com/50"></v-img>
-                </v-avatar>
-              </v-list-item-avatar>
-              <v-list-item-content class="received-message">
-                <v-card color="success" class="flex-none">
-                  <v-card-text class="white--text pa-2 d-flex flex-column">
-                    <span class="text-caption">{{ message.user }} </span>
-                    <span class="align-self-start text-subtitle-1">{{
-                      message.message
-                    }}</span>
-                    <span class="text-caption font-italic align-self-end">{{
-                      message.time
-                    }}</span>
-                  </v-card-text>
-                </v-card>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item v-else :key="message.time">
-              <v-list-item-content class="sent-message justify-end">
-                <v-card color="primary" class="flex-none">
-                  <v-card-text class="white--text pa-2 d-flex flex-column">
-                    <span class="text-subtitle-1 chat-message">{{
-                      message.message
-                    }}</span>
-                    <span class="text-caption font-italic align-self-start">{{
-                      message.time
-                    }}</span>
-                  </v-card-text>
-                </v-card>
-              </v-list-item-content>
-              <v-list-item-avatar class="align-self-start ml-2">
-                <v-img src="https://via.placeholder.com/50"></v-img>
-              </v-list-item-avatar>
-            </v-list-item>
-          </v-card>
-        </v-row>
+        v-if="messages.length == 0"
+        align="center"
+        justify="center"
+        style=" align-self: center;
+    text-align: center"
+        ;
+      >
+        展開激情對話
       </v-container>
-      <!--<ul class="list-group list-group-flush ">-->
-      <!--<small class="text-white">{{ currentUser.username }} is typing</small>
+
+      <v-row justify="space-around d-flex flex-column">
+        <v-card v-for="message in messages" :key="message.time" flat>
+          <v-list-item
+            :key="message.time"
+            v-if="message.from != 'You'"
+            class="blue-grey lighten-5 "
+          >
+            <v-list-item-avatar class="align-self-start mr-2">
+              <v-avatar size="40">
+                <v-img src="https://via.placeholder.com/50"></v-img>
+              </v-avatar>
+            </v-list-item-avatar>
+            <v-list-item-content class="received-message">
+              <v-card color="#99CFEA" class="flex-none">
+                <v-card-text class="white--text pa-2 d-flex flex-column">
+                  <span class="text-caption">{{ message.user }} </span>
+                  <span class="align-self-start text-subtitle-1">{{
+                    message.message
+                  }}</span>
+                  <span class="text-caption font-italic align-self-end">{{
+                    message.time
+                  }}</span>
+                </v-card-text>
+              </v-card>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-else :key="message.time">
+            <v-list-item-content class="sent-message justify-end">
+              <v-card color="primary" class="flex-none">
+                <v-card-text class="white--text pa-2 d-flex flex-column">
+                  <span class="text-subtitle-1 chat-message">{{
+                    message.message
+                  }}</span>
+                  <span class="text-caption font-italic align-self-start">{{
+                    message.time
+                  }}</span>
+                </v-card-text>
+              </v-card>
+            </v-list-item-content>
+            <v-list-item-avatar class="align-self-start ml-2">
+              <v-img src="https://via.placeholder.com/50"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+      </v-row>
+    </v-card>
+    <!--<ul class="list-group list-group-flush ">-->
+    <!--<small class="text-white">{{ currentUser.username }} is typing</small>
         <li
           class="list-group-item"
           v-for="(message, index) in messages"
@@ -67,7 +77,7 @@
           </span>
         </li>
         </ul>-->
-      <!--  <v-virtual-scroll :items="messages" height="300" item-height="64">
+    <!--  <v-virtual-scroll :items="messages" height="300" item-height="64">
         <template v-slot:default="{ item }">
           <v-list-item :key="item">
             <v-list-item-action>
@@ -91,22 +101,37 @@
         </template>
       </v-virtual-scroll>-->
 
-      <div>
-        <form @submit.prevent="send">
-          <div>
-            <input
-              type="text"
-              class="form control"
-              v-model="newMessage"
-              placeholder="Enter message here"
-            />
-          </div>
-        </form>
-      </div>
-
-      <button v-on:click="send">Send</button>
-    </div>
-    <div></div>
+    <v-card
+      style="max-width:1200px; width: 100%; padding: 12px;margin-right: auto;margin-left: auto; "
+      class="scroll-bar blue-grey lighten-5 nl-auto mr-auto"
+      elevation="16"
+    >
+      <form @submit.prevent="send">
+        <div>
+          <v-row>
+            <v-col>
+              <v-text-field
+                type="text"
+                class="form control mx-2"
+                v-model="newMessage"
+                label="展開激情對話"
+              ></v-text-field>
+            </v-col>
+            <v-btn
+              class=" mr-4"
+              elevation="2"
+              color="primary"
+              v-on:click="send"
+              justify="center"
+              align="center"
+              style="text-align:center;display:inline;    align-self: center;
+"
+              >Send</v-btn
+            >
+          </v-row>
+        </div>
+      </form>
+    </v-card>
   </v-container>
 </template>
 
@@ -154,6 +179,7 @@ export default {
     });
   },
   updated() {
+    //scroll to bottom after the chat is sent or updated
     const scrollbar = document.querySelector(".scroll-bar");
     scrollbar.scrollTo(0, scrollbar.scrollHeight);
   },
