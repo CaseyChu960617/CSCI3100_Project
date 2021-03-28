c6f9f028bb33f528a5c4c5dd2511dac72e42d8df
kenny613 <tongkai0613@gmail.com>
2021-03-28 22:59:32 +0800

    Update io

--- frontend/src/views/chat.vue
+++ frontend/src/views/chat.vue
@@ -1,46 +1,59 @@
 <template>
   <v-container fluid class="pa-0">
     <div>
-    <button v-on:click="send">Send</button>
+      <button v-on:click="send">Send</button>
     </div>
-     <div>
-    <button v-on:click="recieve">Reiceve</button>
+    <div>
+      <button v-on:click="receive">receive</button>
     </div>
+    <div></div>
   </v-container>
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
+
 export default {
+  computed: {
+    currentUser() {
+      return this.$store.state.auth.user;
+    },
+  },
+
   data() {
     return {
-        socket:io("http://localhost:9000",{ transports: ['websocket', 'polling', 'flashsocket'] })
-     // isConnected: false,
-     // socketMessage: ''
-    }
+      messages: [],
+      socket: io("http://localhost:9000", {
+        transports: ["websocket", "polling", "flashsocket"],
+        withCredentials: true,
+        extraHeaders: {
+          "my-custom-header": "abcd",
+        },
+      }),
+    };
   },
-
 
   methods: {
-    
     send() {
-        console.log(this.$socket)
-        alert("fuck")
-        this.$socket.emit('send',{
-          user:"Kenny",
-          message:"Hello World"
-     })
+      console.log(this.socket);
+      alert("fuck");
+      this.socket.emit("send", {
+        user: "Kenny",
+        message: "Hi",
+      });
     },
-    recieve() {
-     this.$socket.on('recieve',(data)=>{
-         console.log(data.message)
-     })
-     
-    }
-  },
- 
-  
-  
-  }
 
+    receive() {
+      this.socket.on("receive", (data) => {
+        console.log(data.message);
+      });
+    },
+  },
+  mounted() {
+    this.socket.on("recieve", (data) => {
+      this.messages = [...this.messages, data];
+      console.log(this.messages);
+    });
+  },
+};
 </script>
