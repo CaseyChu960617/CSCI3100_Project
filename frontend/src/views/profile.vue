d387af04317aa7795c1023e74bfe2542cfe0823c
Caseychu0617 <1155093260@link.cuhk.edu.hk>
2021-04-06 16:38:12 +0800

    Update profile.vue

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -1,15 +1,28 @@
 <template>
   <v-container>
-    <div class="container mt-4">
-      <h3>Profile</h3>
-      <p>ID: {{ currentUser.uid }}</p>
-      <p>Username: {{ currentUser.username }}</p>
-      <p>First Name: {{ currentUser.firstname }}</p>
-      <p>Last Name: {{ currentUser.lastname }}</p>
-      <p>Email: {{ currentUser.email }}</p>
-      <p>Gender: {{ currentUser.gender }}</p>
-    </div>
-    <v-btn @click="editProfile()">Edit Profile</v-btn>
+    <v-row>
+      <v-col md="6">
+        <div class="container mt-4">
+          <h3>Profile</h3>
+          <p>ID: {{ currentUser.uid }}</p>
+          <p>Username: {{ currentUser.username }}</p>
+          <p>First Name: {{ currentUser.firstname }}</p>
+          <p>Last Name: {{ currentUser.lastname }}</p>
+          <p>Email: {{ currentUser.email }}</p>
+          <p>Gender: {{ currentUser.gender }}</p>
+        </div>
+        <v-btn @click="editProfile()">Edit Profile</v-btn>
+      </v-col>
+      <v-col md="6"
+        ><div class="container mt-4">
+          <v-avatar color="grey" size="200">
+            <span class="white--text headline">
+              {{ currentUser.username }}</span
+            >
+          </v-avatar>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
