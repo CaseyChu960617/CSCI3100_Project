14ca58c59ee1fb6c9eee686d36557d47f6300500
Caseychu0617 <1155093260@link.cuhk.edu.hk>
2021-04-15 01:07:35 +0800

    Update authJwt.js

--- backend/middlewares/authJwt.js
+++ backend/middlewares/authJwt.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
     let accessToken = req.headers['x-access-token'];
     //console.log(accessToken);
     if (!accessToken) {
-        return res.status(403).json({ error: "No token provided."})
+        return res.status(403).send("No token provided or token expired. Please sign in again.");
     }
     const decodedToken = jwt.verify(accessToken, process.env.JWT_ACC_SECRET);
     req.body['user_id'] = decodedToken['user_id']; //changed Name
