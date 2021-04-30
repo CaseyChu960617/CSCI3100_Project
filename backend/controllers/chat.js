be9ff7bbbbf574c2b9b767834dcaa10cb539505d
Caseychu0617 <1155093260@link.cuhk.edu.hk>
2021-04-30 11:34:52 +0800

    Update backend

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -1,4 +1,5 @@
 const Chat = require("../models/chat");
+const User = require("../models/user");
 const Message = require("../models/message");
 var ObjectId = require("mongoose").Types.ObjectId;
 const mongoose = require("mongoose");
@@ -6,15 +7,22 @@ const mongoose = require("mongoose");
 // getAllChats function.
 exports.getAllChats = async (req, res) => {
   try {
-    // Find all existing chats with this user_id as one of participants
-    Chat.find({ $or: [{ userA: req.params["user_id"] }, { userB: req.params["user_id"] }] })
-    .sort({ createdAt: -1 })
-    .select("_id userA userB")
-    .populate("userA userB", "_id username profileImage")
-    .exec()
-    .then((docs) => {
-        res.status(200).send(docs);
-    });
+
+    const user = await User.findOne({ _id: ObjectId(req.params["user_id"])});
+
+    if (user) {
+      // Find all existing chats with this user_id as one of participants
+      Chat.find({ $or: [{ userA: req.params["user_id"] }, { userB: req.params["user_id"] }] })
+      .sort({ createdAt: -1 })
+      .select("_id userA userB")
+      .populate("userA userB", "_id username profileImage")
+      .exec()
+      .then((docs) => {
+          res.status(200).send(docs);
+      });
+    } else　{
+      res.status(400).send({ message: "User not found."});
+    }
   } catch(err) {
     return res.status(400).send({ message: err.message });
   }
@@ -35,7 +43,11 @@ exports.getOneChatById = async (req, res) => {
       },
     },
   ];
+
   try {
+
+    
+
     // Find the chat and all the messages in the chat with this chat_id.
     await Chat.findOne({ _id: req.params["chat_id"] })
       .select("userA userB messages")
@@ -80,6 +92,11 @@ exports.getOneChat = async (req, res) => {
   ];
 
   try {
+
+    const userA = User.findOne({ _id: ObjectId(user_id_1)});
+    const userB = User.findOne({ _id: ObjectId(user_id_2)});
+
+    if (userA && userB) {
     // Find one chat with two user_id.
     await Chat.findOne({
       $or: [
@@ -92,19 +109,22 @@ exports.getOneChat = async (req, res) => {
     .exec()
     .then((doc) => {
       // If the chat does not exist, create a chat.
-      if (!doc) {
-        Chat.create(
-          {
-            userA: user_id_1,
-            userB: user_id_2,
-            messages: [],
-            createdAtDate: new Date().getTime(),
-          },
-          (doc) => {
-            res.status(200).send(doc);
-          });
-      } else res.status(200).send(doc);
-    });
+        if (!doc) {
+          Chat.create(
+            {
+              userA: user_id_1,
+              userB: user_id_2,
+              messages: [],
+              createdAtDate: new Date().getTime(),
+            },
+            (doc) => {
+              res.status(200).send(doc);
+            });
+        } else res.status(200).send(doc);
+      });
+    } else {
+      res.status(400).send({ message: "User not found." });
+    }
   } catch(err) {
     res.status(400).send({ message: err.message });
   }
