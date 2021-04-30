6fa1d79dd6bf4b7605a5a2a4135e7075b7ac56f2
hccheun8 <1155108692@link.cuhk.edu.hk>
2021-04-30 11:34:16 +0800

    Revert "Update chat"
    This reverts commit e6adc460bf0a6254d5b9a4b5753ce5490a4ba8b8.

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -112,19 +112,23 @@ exports.getOneChat = async (req, res) => {
 
 // Send message function.
 exports.sendMessage = async (data) => {
+  try {
     // Create a new message and push the objectId 
     // to the messages list in corresponding chat
     const newMessage = new Message(
       {
-        sender: ObjectId(data.sender._id),
+        sender: new ObjectId(data.sender._id),
         message: data.message,
         timestamp: data.timestamp,
       });
 
     newMessage.save();
-    console.log(newMessage._id);
+
     Chat.findOneAndUpdate(
       { _id: data.chatId },
-      { $push: { messages: newMessage._id } }, (err) => {
-      });
+      { $push: { messages: newMessage._id } });
+      
+  } catch(err) {
+
+  }
 }
